210716b54e2f68fc093a542b71c06a3fe9b165b6
Sidivo <ovidijus0808@gmail.com>
2026-06-19 14:25:49 +0300

    refactor: rebuild fct_trials_summary as aggregate over fct_trials

--- clinical_dbt/models/marts/fct_trials_summary.sql
+++ clinical_dbt/models/marts/fct_trials_summary.sql
@@ -1,21 +1,11 @@
 {{ config(external_location='s3://clinical-data-platform-omiliauskas/marts/fct_trials_summary/') }}
 
-WITH trials AS (
-
-    SELECT nct_id, overall_status, enrollment_count, 'heart_disease' AS condition
-    FROM {{ ref('stg_trials_heart_disease') }}
-
-    UNION ALL
-
-    SELECT nct_id, overall_status, enrollment_count, 'diabetes' AS condition
-    FROM {{ ref('stg_trials_diabetes') }}
-
-)
-
 SELECT
-    condition,
-    overall_status,
-    COUNT(*)              AS trials_count,
-    SUM(enrollment_count) AS total_enrollment
-FROM trials
-GROUP BY condition, overall_status
+    c.condition,
+    s.overall_status,
+    COUNT(*)                AS trials_count,
+    SUM(f.enrollment_count) AS total_enrollment
+FROM {{ ref('fct_trials') }} f
+JOIN {{ ref('dim_condition') }} c ON f.condition_key = c.condition_key
+JOIN {{ ref('dim_status') }}    s ON f.overall_status_key = s.overall_status_key
+GROUP BY c.condition, s.overall_status
